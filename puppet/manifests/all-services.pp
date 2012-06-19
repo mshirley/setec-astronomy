6bd5c827bea5c4339488ce153942e611c786be5e
Mark Shirley <mshirley@gmail.com>
2012-06-18 21:45:01 -0600

    cleanup and simplification

---
+++ puppet/manifests/all-services.pp
@@ -0,0 +1,25 @@
+$s3_base_url = "https://s3.amazonaws.com/setec-astronomy-s3/src/setec-astronomy"
+
+# Package install
+$basepkgs = [ "vim", "openssh-server", "tor", "ejabberd", "openvpn", "bind9" ] 
+package { $basepkgs : ensure => "installed" } 
+
+# stop and disable 
+service { [ "tor", "ejabberd", "openvpn", "bind9" ] : 
+  ensure => "stopped",
+  enable => "false"
+}
+
+# download and install ejabberd.cfg
+exec { "dl_ejabberd_cfg":
+  command => "wget #{$s3_base_url}/conf/ejabberd/ejabberd.cfg -O /tmp/ejabberd.cfg",
+  cwd     => "/tmp/",
+  creates => "/tmp/ejabberd.cfg",
+  path    => ["/usr/bin", "/usr/sbin"]
+}
+
+file { "/etc/ejabberd/ejabberd.cfg":
+  source => "/tmp/ejabberd.cfg",
+  require => Exec["dl_ejabberd_cfg"]
+}
+
