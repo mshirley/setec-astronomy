768a3c10b7c63e102951fbd79d168aacba53ff69
Mark Shirley <mshirley@gmail.com>
2012-06-18 18:12:05 -0600

    disable services

--- puppet/manifests/sa-all-svc-pkg-install.pp
+++ puppet/manifests/sa-all-svc-pkg-install.pp
@@ -2,3 +2,24 @@
 
 $basepkgs = [ "vim", "openssh-server", "tor", "ejabberd", "openvpn", "bind9" ] 
 package { $basepkgs : ensure => "installed" } 
+
+service { "tor" : 
+  ensure => "stopped",
+  enable => "false"
+}
+
+service { "ejabberd" :
+  ensure => "stopped",
+  enable => "false"
+}
+
+service { "openvpn" :
+  ensure => "stopped",
+  enable => "false"
+}
+
+service { "bind9" :
+  ensure => "stopped",
+  enable => "false"
+}
+
