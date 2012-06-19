ef15144eea8efbe681ebe7d6b4cb49e7265598e0
Mark Shirley <mshirley@gmail.com>
2012-06-18 20:21:49 -0600

    file is being created but not overwriting, trying /tmp

--- puppet/manifests/sa-all-svc-cfg.pp
+++ puppet/manifests/sa-all-svc-cfg.pp
@@ -1,6 +1,6 @@
 # ejabberd.conf
 exec { "wget https://s3.amazonaws.com/setec-astronomy-s3/src/setec-astronomy/conf/ejabberd/ejabberd.cfg":
-  cwd     => "/etc/ejabberd/",
+  cwd     => "/tmp/",
   creates => "/etc/jabberd/ejabberd.cfg",
   path    => ["/usr/bin", "/usr/sbin"]
 }
