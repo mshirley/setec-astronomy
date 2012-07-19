47c66d2da322674c33d7e0c5afc4d21976a02f17
Mark Shirley <mshirley@gmail.com>
2012-07-18 20:56:15 -0600

    basic functions from the api are working, i will transfer my knowledge to usercontroller.rb

--- controller/aws-sdk-dev.rb
+++ controller/aws-sdk-dev.rb
@@ -1,8 +1,8 @@
 require 'rubygems' 
 require 'aws-sdk'
 
-cfnpath = "/opt/aws-cloudformatino-tools/"
 credfile = "#{ENV['HOME']}/.ec2/creds"
+templatefile = "#{ENV['HOME']}/src/setec-astronomy/aws/setec-astronomy.template"
 
 def readcreds(credfile)
   puts "Reading EC2 credentials file, #{credfile}"
@@ -22,17 +22,74 @@ awsaccesskey, awssecretkey = readcreds(credfile)
 
 cfm = AWS::CloudFormation.new( :access_key_id => awsaccesskey, :secret_access_key => awssecretkey)
 
-puts cfm.methods.sort
+#puts cfm.methods.sort
 
+
+# this will be used to determine if a stack exists, link with creation function
+if !cfm.stacks['setec-astronomy'].exists?
+  puts "doesn't exist"
+else
+  puts "it exists"
+end
+
+# this will create a new stack using the existing template
+puts "creating stack"
+template = File.read(templatefile)
+cfm.stacks.create('setec-astronomy', template, :parameters => { 'KeyName' => 'setec-astronomy' })
 setecstack = cfm.stacks['setec-astronomy']
+puts "create signal sent"
+stackstatus = setecstack.status
+puts "current status is #{stackstatus}"
 
-puts setecstack.methods.sort
+while stackstatus == "CREATE_IN_PROGRESS" do
+  puts "creation still in progress, waiting..."
+  break if stackstatus == "CREATE_COMPLETE"
+  sleep 5 
+  stackstatus = setecstack.status
+end
+
+#setecstack = cfm.stacks.create('name', template, :parameters => {
+#  'KeyName' => 'setec-astronomy',
+#})
+
+
+#puts setecstack.methods.sort
 
 # this is what we would need to look at after creating a stack to determine when it is finished
 # CREATE_COMPLETE
-puts "Status is #{setecstack.status}"
+# DELETE_IN_PROGRESS
+# CREATE_IN_PROGRESS
+
+#puts "Status is #{setecstack.status}"
+puts "pulling ip address of stack"
+stackip = setecstack.outputs[2].value
+while stackip.nil? do
+  puts "no ip address yet, waiting"
+  #break if !stackip.nil?
+  sleep 3
+  stackip = setecstack.outputs[2].value
+end
+
+puts "ip address of stack is #{stackip}"
+
+# this is how you pull the ip address from a stack.  this is specific to the template i'm currently using. 
+#puts "ip is #{setecstack.outputs[2].value}"
+
+# this will provide a list of events for the stack
+setecstack.events.each do |event|
+    puts "#{event.timestamp}: #{event.resource_status}"
+end
+#puts setecstack.methods.sort
 
 
+# this should delete the stack
+# puts "Deleting stack" 
+# setecstack.delete
+
+# setecstack.outputs.each do |outputs|
+#   puts outputs.key
+#   puts outputs.value
+# end
 
 # FROM TFM http://docs.amazonwebservices.com/AWSRubySDK/latest/frames.html
 ## enumerating all stack objects
