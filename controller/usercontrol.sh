014798e36737c0fbd95e3fc75032d9eb886d0131
Mark Shirley <mshirley@gmail.com>
2012-08-04 00:04:57 -0600

    the ssh key will be used for doing secure copy of remote openvpn files.  relies on ssh-add and truecrypt to be functioning.
    diff --git a/controller/usercontrol.sh b/controller/usercontrol.sh
    index 24b2234..fd0c33b 100644
    --- a/controller/usercontrol.sh
    +++ b/controller/usercontrol.sh
    @@ -5,4 +5,13 @@ unset https_proxy
     unset HTTPS_PROXY
     unset ftp_proxy
     unset FTP_PROXY
    +# setting key for doing scp
    +if [ -f $SSHKEY ]
    +then
    +  ssh-add $SSHKEY
    +else
    +  echo "mounting tcvolume $TCVOLUME"
    +  truecrypt $TCVOLUME /media/truecrypt1/
    +  ssh-add $SSHKEY
    +fi
     ruby usercontrol.rb

--- controller/usercontrol.sh
+++ controller/usercontrol.sh
@@ -5,4 +5,13 @@ unset https_proxy
 unset HTTPS_PROXY 
 unset ftp_proxy 
 unset FTP_PROXY 
+# setting key for doing scp
+if [ -f $SSHKEY ]
+then
+  ssh-add $SSHKEY
+else
+  echo "mounting tcvolume $TCVOLUME"
+  truecrypt $TCVOLUME /media/truecrypt1/
+  ssh-add $SSHKEY
+fi
 ruby usercontrol.rb
